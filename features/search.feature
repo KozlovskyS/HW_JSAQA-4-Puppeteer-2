7edf37869103938ba8f3e850f5aafcbcd452fbff
KozlovskyS <skozmail@gmail.com>
2025-06-09 13:36:12 +0300

    added cucumber feature

--- features/search.feature
+++ features/search.feature
@@ -1,8 +1,34 @@
 Feature: GoToCinema tests
     Scenario: Successful standart ticket booking
         Given user is on booking page
-        When user choice day
-        When user choice hall 
-        When user choice seat
+        When user choice day "3"
+        When user choice hall "217"
+        When user choice seat row "3" coll "6"
         When user click on button booking
         Then user sees the text "Вы выбрали билеты"
+
+    Scenario: Successful get QR-code for standart ticket booking
+        Given user is on booking page
+        When user choice day "5"
+        When user choice hall "217"
+        When user choice seat row "4" coll "2"
+        When user click on button booking
+        When user click on button booking
+        Then user sees the text "Электронный билет"
+        
+    Scenario: Successful VIP ticket booking
+        Given user is on booking page
+        When user choice day "3"
+        When user choice hall "199"
+        When user choice seat row "4" coll "6"
+        When user click on button booking
+        Then user sees the text "Вы выбрали билеты"
+
+    Scenario: Sade path - Booking a reserved ticket
+         Given user is on booking page
+        When user choice day "5"
+        When user choice hall "217"
+        When user choice seat row "4" coll "2"
+        When user click on button booking
+        Then user sees disabled button ".acceptin-button"
+    
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -5,10 +5,11 @@ const { Given, When, Then, Before, After } = require("cucumber");
 const { putText, getText } = require("../../lib/commands.js");
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
   const page = await browser.newPage();
   this.browser = browser;
   this.page = page;
+  this.page.waitForNavigation(30000);
 });
 
 After(async function () {
@@ -17,40 +18,53 @@ After(async function () {
   }
 });
 
-Given ("user is on booking page", async function () {
-   return await this.page.goto("https://qamid.tmweb.ru/client/index.php", {
+Given(
+  "user is on booking page",
+  { wrapperOptions: { retry: 1 }, timeout: 40000 },
+  async function () {
+    await this.page.goto("https://qamid.tmweb.ru/client/index.php", {
       setTimeout: 30000,
     });
-  });
+    await this.page.waitForSelector("h1");
+  },
+  50000
+);
 
-When("user choice day", async function () {
-  // Write code here that turns the phrase above into concrete actions
-  const choiceDay = ".page-nav > a:nth-child(3)"; // выбор дня - 3-й день от сегодня
-  return await clickElement(this.page, choiceDay);
-}, 10000);
+When(
+  "user choice day {string}",
+  { wrapperOptions: { retry: 1 }, timeout: 40000 },
+  async function (string) {
+    const choiceDay = `.page-nav > a:nth-child('${string}')`; // выбор дня  от сегодня
+    return await clickElement(this.page, choiceDay);
+  },
+  10000
+);
 
-When("user choice hall", async function () {
-  // Write code here that turns the phrase above into concrete actions
-  const choiceHall = ".movie-seances__time[href='#'][data-seance-id='217']"; // ЗалЗал90 "Сталкер"
+When("user choice hall {string}", async function (string) {
+  const choiceHall = `.movie-seances__time[href='#'][data-seance-id='${string}']`; // ЗалЗал90 "Сталкер"
   await clickElement(this.page, choiceHall);
   await this.page.waitForSelector(".buying__info");
 });
 
-When("user choice seat", async function () {
-  // Write code here that turns the phrase above into concrete actions
-  const choiceSeat = "div:nth-child(2) span:nth-child(6)"; //ряд 2 место 6
+When("user choice seat row {string} coll {string}", async function (row, coll) {
+  const choiceSeat = `div:nth-child(${row}) span:nth-child(${coll})`; //ряд row место coll
   await clickElement(this.page, choiceSeat);
 });
 
 When("user click on button booking", async function () {
-  // Write code here that turns the phrase above into concrete actions
   await clickElement(this.page, ".acceptin-button");
 });
 
-Then("user sees the text {string}", async function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  const expected = "Вы выбрали билеты:";
+Then("user sees the text {string}", async function (mess) {
+  const expected = mess;
   const actual = await getText(this.page, ".ticket__check-title");
 
   await expect(actual).toContain(expected);
+});
+
+Then("user sees disabled button {string}", async function (string) {
+  const expected = true;
+  const actual = await this.page.$eval(`${string}`, (btn) => btn.disabled);
+
+  await expect(actual).toBe(expected);
 });
